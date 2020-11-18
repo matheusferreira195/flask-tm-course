76a7f0999144bdc0103cc294c49d8e459012a371
matheusAuraconsultoria <matheus@auraconsultoria.com.br>
2020-11-18 19:53:57 -0300

    Init flask

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Labore incididunt aute duis anim ut consequat est deserunt et nostrud ipsum.',
+            'author': 'Brad Traversy',
+            'create_date': '11-11-2011'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Labore incididunt aute duis anim ut consequat est deserunt et nostrud ipsum.',
+            'author': 'Brad Traversy',
+            'create_date': '11-11-2011'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Labore incididunt aute duis anim ut consequat est deserunt et nostrud ipsum.',
+            'author': 'Brad Traversy',
+            'create_date': '11-11-2011'
+        },
+    ]
+
+    return articles
